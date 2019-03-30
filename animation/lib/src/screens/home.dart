7337f7b00c696cb2597f3942b35d29a0fd4dc632
Richard Rouzeau <richard@evigy.com>
2019-03-30 10:09:24 -0400

    Cat animated forward & reverse

--- animation/lib/src/screens/home.dart
+++ animation/lib/src/screens/home.dart
@@ -5,17 +5,57 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  @override
+  void initState() {
+    super.initState();
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+
+    catAnimation = Tween(
+      begin: 0.0,
+      end: 100.0,
+    ).animate(
+      CurvedAnimation(
+        parent: catController,
+        curve: Curves.easeIn,
+      ),
+    );
+  }
+
+  handleTap() {
+    catAnimation.status == AnimationStatus.completed
+        ? catController.reverse()
+        : catController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Pussycat in a box'),
-        ),
-        body: buildAnimation());
+      appBar: AppBar(
+        title: Text('Pussycat in a box'),
+      ),
+      body: GestureDetector(
+        child: buildAnimation(),
+        onTap: handleTap,
+      ),
+    );
   }
 
   Widget buildAnimation() {
-    return Cat();
+    return AnimatedBuilder(
+        animation: catAnimation,
+        child: Cat(),
+        builder: (context, child) {
+          return Container(
+            margin: EdgeInsets.only(top: catAnimation.value),
+            child: child,
+          );
+        });
   }
 }
