fb658b53b82d199f2dea3834fed3ce002b46676b
Richard Rouzeau <richard@evigy.com>
2019-04-04 17:16:37 -0400

    Box animation done closer to css example

--- animation/lib/src/screens/home.dart
+++ animation/lib/src/screens/home.dart
@@ -6,33 +6,73 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> with TickerProviderStateMixin {
-  Animation<double> catAnimation;
-  AnimationController catController;
+  Animation<double> _catAnimation;
+  Animation<double> _flapAnimation;
+  Animation<double> _boxAnimation;
+  AnimationController _catController;
+  AnimationController _boxController;
+  int _direction = 1;
+
   Color boxColor = Color(0xFFCB922E);
 
   @override
   void initState() {
     super.initState();
-    catController = AnimationController(
+    _catController = AnimationController(
       duration: Duration(milliseconds: 200),
       vsync: this,
     );
 
-    catAnimation = Tween(
-      begin: -35.0,
-      end: -82.0,
+    _catAnimation = Tween(
+      begin: -33.0,
+      end: -80.0,
     ).animate(
       CurvedAnimation(
-        parent: catController,
+        parent: _catController,
         curve: Curves.easeIn,
+      ),
+    );
+
+    _boxController = AnimationController(
+      duration: Duration(milliseconds: 80),
+      vsync: this,
+    );
+
+    _flapAnimation = Tween(
+      begin: 0.0,
+      end: 0.2,
+    ).animate(
+      CurvedAnimation(
+        parent: _boxController,
+        curve: Curves.bounceOut,
+        reverseCurve: Curves.bounceOut,
+      ),
+    );
+
+    _boxAnimation = Tween(
+      begin: 0.0,
+      end: 0.05,
+    ).animate(
+      CurvedAnimation(
+        parent: _boxController,
+        curve: Curves.linear,
       ),
     );
   }
 
-  handleTap() {
-    catAnimation.status == AnimationStatus.completed
-        ? catController.reverse()
-        : catController.forward();
+  Future<void> handleTap() async {
+    _catAnimation.status == AnimationStatus.completed
+        ? _catController.reverse()
+        : _catController.forward();
+
+    try {
+      for (var i = 0; i < 4; i++) {
+        await _boxController.forward().orCancel;
+        await _boxController.reverse().orCancel;
+        _direction = -_direction;
+      }
+      _direction = -_direction;
+    } on TickerCanceled {}
   }
 
   @override
@@ -56,58 +96,57 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
     );
   }
 
-  Widget boxFlapLeft() {
-    return Positioned(
-      left: -8,
-      top: 0,
-      child: Transform.rotate(
-        angle: 0.2,
-        alignment: Alignment.topRight,
-        child: Container(
-          width: 10,
-          height: 100,
-          color: boxColor,
-        ),
-      ),
-    );
-  }
-
-  Widget boxFlapRight() {
-    return Positioned(
-      left: 198,
-      top: 0,
-      child: Transform.rotate(
-        angle: 6.0,
-        alignment: Alignment.topLeft,
-        child: Container(
-          width: 10,
-          height: 100,
-          color: boxColor,
-        ),
-      ),
+  Widget boxFlap(double left, double right, double angle, Alignment alignment,
+      int direction) {
+    return AnimatedBuilder(
+      animation: _boxController,
+      builder: (context, builder) {
+        return Positioned(
+          left: left,
+          right: right,
+          top: 0,
+          child: Transform.rotate(
+            angle: angle + (_direction * _flapAnimation.value),
+            alignment: alignment,
+            child: Container(
+              width: 10,
+              height: 100,
+              color: boxColor,
+            ),
+          ),
+        );
+      },
     );
   }
 
   Widget box() {
-    return Stack(
-      overflow: Overflow.visible,
-      children: <Widget>[
-        boxBody(),
-        boxFlapLeft(),
-        boxFlapRight(),
-      ],
+    return AnimatedBuilder(
+      animation: _boxController,
+      builder: (context, builder) {
+        return Transform.rotate(
+          angle: (_direction * _boxAnimation.value),
+          child: Stack(
+            overflow: Overflow.visible,
+            children: <Widget>[
+              boxBody(),
+              boxFlap(-8, 200, 0.2, Alignment.topRight, 1),
+              boxFlap(200, -8, 6.0, Alignment.topLeft, -1)
+            ],
+          ),
+        );
+      },
     );
   }
 
   Widget buildCatAnimation() {
     return AnimatedBuilder(
-        animation: catAnimation,
+        animation: _catController,
         child: Cat(),
         builder: (context, child) {
           return Positioned(
             left: 0.0,
             right: 0.0,
-            top: catAnimation.value,
+            top: _catAnimation.value,
             child: child,
           );
         });
