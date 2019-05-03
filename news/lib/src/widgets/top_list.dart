1e5b9d0a8fc01d5a82a914a60703370270e516aa
Richard Rouzeau <richard@evigy.com>
2019-05-03 11:56:25 -0400

    Back to previous structure - Scroll-up still buggy

--- news/lib/src/widgets/top_list.dart
+++ news/lib/src/widgets/top_list.dart
@@ -2,47 +2,41 @@ import 'package:flutter/material.dart';
 // import 'dart:collection';
 import '../blocs/stories_provider.dart';
 import '../models/item_model.dart';
-// import '../resources/repository.dart';
 
-class TopList extends StatefulWidget {
-  @override
-  _TopListState createState() => _TopListState();
-}
-
-class _TopListState extends State<TopList> {
-  // List<int> _topList;
+class TopList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final StoriesBloc bloc = StoriesProvider.of(context);
-    bloc.getTopNews();
-    return RefreshIndicator(
-      onRefresh: () => bloc.resfreshTopList(),
-      child: StreamBuilder<List<int>>(
-          stream: bloc.topList,
-          builder: (context, snapshot) {
-            return snapshot.hasData
-                ? ListView.builder(
-                    itemBuilder: (context, int index) {
-                      final Future<ItemModel> fetchedItem =
-                          bloc.getNewsItem(snapshot.data[index]);
-                      return FutureBuilder<ItemModel>(
-                          key: Key(snapshot.data[index].toString()),
-                          future: fetchedItem,
-                          builder: (context, snapshot) {
-                            return snapshot.hasData
-                                ? buildItem(snapshot.data, index)
-                                : Text('Loading...');
-                          });
-                    },
-                  )
-                : Center(
-                    child: CircularProgressIndicator(),
-                  );
-          }),
-    );
+    return StreamBuilder<List<int>>(
+        stream: bloc.topList,
+        builder: (context, snapshot) {
+          return snapshot.hasData
+              ? ListView.builder(
+                  itemBuilder: (context, int index) {
+                    return FutureBuilder<ItemModel>(
+                        key: Key(snapshot.data[index].toString()),
+                        future: bloc.getNewsItem(snapshot.data[index]),
+                        builder: (context, snapshot) {
+                          switch (snapshot.connectionState) {
+                            case ConnectionState.none:
+                            case ConnectionState.active:
+                            case ConnectionState.waiting:
+                              print('${snapshot.connectionState} for $index');
+                              return Text('Loading...');
+                            case ConnectionState.done:
+                              return buildItem(snapshot.data, index);
+                          }
+                        });
+                  },
+                )
+              : Center(
+                  child: CircularProgressIndicator(),
+                );
+        });
   }
 
   Widget buildItem(ItemModel item, int index) {
+    print('Building row $index');
     return ListTile(
       key: Key(index.toString()),
       leading: CircleAvatar(
